f95d3335a3e6504d9242e8b5c53a589694d34634
My8921 <ahmadali43a5@gmail.com>
2023-11-04 09:34:52 -0700

    Home page D1

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ProductList from './Feature/product-list/ProductList'
+import Home from './Pages/Home'
+
+function App() {
+ 
+  return (
+    <>
+    
+<Home></Home>
+
+  
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,20 @@
+import Navbar from "../Feature/Navbar/Navbar";
+import ProductList from "../Feature/product-list/ProductList";
+
+
+
+function Home() {
+    return ( 
+<div>
+
+
+    <Navbar>  </Navbar>
+
+
+    <ProductList></ProductList> 
+</div>
+
+     );
+}
+
+export default Home;
